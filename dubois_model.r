898425a4b0727b73a2308765be0ca8fe8e287b83
Chris Gaylor <crgaylor@gmail.com>
2014-06-25 11:58:24 -0700

    First actual project for CSSS

---
+++ dubois_model.r
@@ -0,0 +1,18 @@
+#Applying the simulation on p. 242 to R. 
+
+i = seq(0:9)
+j = seq(0:9)
+
+beta_11 = c(0,3,0)
+beta_12 = c(-1,0,0)
+beta_21 = c(-1,0,0)
+beta_22 = c(0,2,0)
+
+s = c(s0, s1, s2)
+
+for ( k in i) {
+	for ( l in k) {
+		 
+	}
+}
+
