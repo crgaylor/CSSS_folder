a8b1320f75c393e998243208e821b07b9eb94b8b
Chris Gaylor <crgaylor@gmail.com>
2014-06-30 08:29:47 -0700

    new day

--- dubois_model.r
+++ dubois_model.r
@@ -1,23 +1,22 @@
 #Applying the simulation on p. 242 to R. 
 
-i = seq(0:9)
-j = seq(0:9)
+i = seq(1:10)
+j = seq(1:10)
 
 beta_11 = c(0,3,0)
-print(beta_11)
+# print(beta_11)
 beta_12 = c(-1,0,0)
-print(beta_12)
+# print(beta_12)
 beta_21 = c(-1,0,0)
-print(beta_21)
+# print(beta_21)
 beta_22 = c(0,2,0)
-print(beta_22)
+# print(beta_22)
 
 
 s = rep(1, 3)
 
 for ( k in i) {
 	for ( l in j) {
-		 
+		 if (
 	}
 }
-
